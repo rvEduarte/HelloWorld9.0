4b2da191b27ea6371544bfce12e3931af5728ff5
LAPTOP-LPQ843GE <hydropumpppp.012@gmail.com>
2024-07-02 21:13:32 +0800

    070224 Updates: Editing Scene 1,2 and Mode Selection (Not Fixed)

---
+++ Assets/TabSettingMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabSettingMenu : MonoBehaviour
+{
+
+    public GameObject[] Tabs;
+    public Image[] tabButtons;
+    public Sprite inactiveTabBG, activeTabBG;
+    public Vector2 inactiveBtnSize, activeBtnSize;
+
+
+    public void Switch2Tab(int tabID)
+    {
+        foreach (GameObject go in Tabs)
+        {
+            go.SetActive(false);
+        }
+
+        Tabs[tabID].SetActive(true);
+
+        foreach (Image img in tabButtons)
+        {
+            img.sprite = inactiveTabBG;
+            img.rectTransform.sizeDelta = activeBtnSize;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
