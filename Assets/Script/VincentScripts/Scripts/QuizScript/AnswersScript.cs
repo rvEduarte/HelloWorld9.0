b00af8486bc7c2ab548f98c4c1af078780d609d9
DESKTOP-MVDTUTR\Rv <rveduarte@gmail.com>
2024-07-02 16:33:58 -0700

    07/2/2024 Updates: working on ph3

--- Assets/Script/VincentScripts/Scripts/QuizScript/AnswersScript.cs
+++ Assets/Script/VincentScripts/Scripts/QuizScript/AnswersScript.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnswersScript : MonoBehaviour
 {
     public bool isCorrect = false;
     public QuizManage quizManager;
+
     public void Answer()
     {
         if (isCorrect)
         {
+            
             Debug.Log("Correst Answer");
             quizManager.correct();
         }
