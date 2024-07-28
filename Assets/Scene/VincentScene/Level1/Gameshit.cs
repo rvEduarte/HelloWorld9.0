da844db49879bcc856a60141210667383922cbc7
DESKTOP-MVDTUTR\Rv <rveduarte@gmail.com>
2024-07-28 10:19:08 -0700

    07/28/Working on LevelSELECTION

---
+++ Assets/Scene/VincentScene/Level1/Gameshit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameShit
+{
+    public string Name;
+    public string Description;
+    public int Level;
+
+    public GameShit()
+    {
+        this.Name = "rv";
+        this.Description = "Description";
+        this.Level = 1;
+    }
+}
+
