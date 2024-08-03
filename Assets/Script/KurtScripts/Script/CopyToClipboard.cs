e91df2cdc6eb515abab8c7d795f5c4aa2435a068
LAPTOP-NOJAEQB4\Kurt <ritsushigeo41@gmail.com>
2024-08-03 10:01:23 +0800

    030824 Updates: Copy To Clipboard Successfully Worked

--- Assets/Script/KurtScripts/Script/CopyToClipboard.cs
+++ Assets/Script/KurtScripts/Script/CopyToClipboard.cs
@@ -1,20 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class CopyToClipboard : MonoBehaviour
 {
-    public Text codeText; // Use TextMeshProUGUI if using TextMeshPro
+    [SerializeField] private GameObject output;
+    public TextMeshProUGUI codeText; // Use TextMeshProUGUI if using TextMeshPro
     public Button copyButton;
+
+    public float timeToDestroy = 5f; // Ensure you have a default value for timeToDestroy
 
     void Start()
     {
         copyButton.onClick.AddListener(CopyCodeToClipboard);
+        output.SetActive(false); // Initially hide the output
     }
 
-    
     void CopyCodeToClipboard()
     {
         if (codeText != null)
@@ -22,11 +24,23 @@ public class CopyToClipboard : MonoBehaviour
             // Copy text to clipboard
             GUIUtility.systemCopyBuffer = codeText.text;
             Debug.Log("Code copied to clipboard: " + codeText.text);
+            output.SetActive(true);
+
+            // Start the coroutine to destroy the game object after a delay
+            StartCoroutine(DestroyAfterDelay());
         }
         else
         {
             Debug.LogError("Code text is not assigned.");
         }
+    }
 
+    private IEnumerator DestroyAfterDelay()
+    {
+        // Wait for the specified amount of time
+        yield return new WaitForSeconds(timeToDestroy);
+
+        // Destroy the game object
+        Destroy(gameObject);
     }
 }
