c8656d92c0c5479b94271ece09047f9f62e5c2f6
DESKTOP-MVDTUTR\Rv <rveduarte@gmail.com>
2024-10-03 21:49:15 -0700

    qwe

---
+++ Assets/GetPlayerProgression.cs
@@ -0,0 +1,83 @@
+using LootLocker.Requests;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GetPlayerProgression : MonoBehaviour
+{
+    public TMP_Text level;
+    public TMP_InputField input;
+    string progressionKey = "levelprogress";
+    private void Start()
+    {
+        RegisterPlayerProgression();
+        CheckXp();
+    }
+    public void CheckXp()
+    {
+        LootLockerSDKManager.GetPlayerInfo((response) =>
+        {
+            if (response.success)
+            {
+                Debug.Log("SUCCESSFUL GET THE XP");
+                level.text = response.level.ToString();
+            }
+            else
+            {
+                Debug.Log("PAKSHET" + response.errorData);
+            }
+           
+        });
+        LootLockerSDKManager.GetPlayerProgression(progressionKey, response =>
+        {
+            if (response.success)
+            {
+                Debug.Log("The player is currently level" + response.step.ToString());
+                if (response.next_threshold != null)
+                {
+                    Debug.Log("Points needed to reach next level:" + (response.next_threshold - response.points).ToString());
+                }
+            }
+            else
+            {
+                Debug.Log("Failed: " + response.errorData);
+            }
+        });
+    }
+    private void RegisterPlayerProgression()
+    {
+        LootLockerSDKManager.RegisterPlayerProgression(progressionKey, (response) =>
+        {
+            if (response.success)
+            {
+                Debug.Log("Progression registered");
+            }
+            else
+            {
+                Debug.Log("Error registering progression");
+            }
+        });
+    }
+
+    public void GiveXP()
+    {
+        int pointsAmount = 1;
+        LootLockerSDKManager.AddPointsToPlayerProgression(progressionKey, (ulong)pointsAmount, (response) =>
+        {
+            if (response.success)
+            {
+                Debug.Log("Points added to progression");
+                // If the player leveled up, the count of awarded_tiers will be greater than 0
+                if (response.awarded_tiers.Count > 0)
+                {
+                    Debug.Log("Player leveled up");
+                }
+            }
+            else
+            {
+                Debug.Log("Error adding points to progression");
+            }
+        });
+    }
+}
