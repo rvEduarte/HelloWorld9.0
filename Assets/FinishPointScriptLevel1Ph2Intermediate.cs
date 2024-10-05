3563149b1a14548155a4fdacb10312c880e13e9e
DESKTOP-MVDTUTR\Rv <rveduarte@gmail.com>
2024-10-05 14:01:14 -0700

    for merging

---
+++ Assets/FinishPointScriptLevel1Ph2Intermediate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPointScriptLevel1Ph2Intermediate : MonoBehaviour
+{
+    [SerializeField] public GameObject gameCompletion;
+
+    public RunningTimerLevel1Ph1Intermediate timer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameCompletion.SetActive(true);
+            TriggerTutorial.disableMove = false; //disable Move
+            TriggerTutorial.disableJump = true; //disable jumping
+            timer.isPicked = true;
+        }
+    }
+}
