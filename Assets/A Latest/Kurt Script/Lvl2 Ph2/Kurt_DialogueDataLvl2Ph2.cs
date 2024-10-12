09dc728c313b6353f1a2843dcba1e003c6da2faf
LAPTOP-NOJAEQB4\Kurt <ritsushigeo41@gmail.com>
2024-10-12 22:39:27 +0800

    burat si karl eh

---
+++ Assets/A Latest/Kurt Script/Lvl2 Ph2/Kurt_DialogueDataLvl2Ph2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KurtMessageLvl2Ph2
+{
+    public int actorId;
+    public string message;
+}
+
+[System.Serializable]
+public class KurtActorLvl2Ph2
+{
+    public string name;
+    public Sprite sprite;
+}
---
+++ Assets/Kurt_DialogueTriggerLvl2Ph2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kurt_DialogueTriggerLvl2Ph2 : MonoBehaviour
+{
+    public KurtMessageLvl2Ph2[] message;
+    public KurtActorLvl2Ph2[] actor;
+
+    private bool hasTriggered = false; // To track if the conversation has already occurred
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !hasTriggered) // Only trigger if conversation hasn't happened
+        {
+            StartDialogue();
+        }
+    }
+
+    public void StartDialogue()
+    {
+        hasTriggered = true; // Set flag to true so it only triggers once
+        FindObjectOfType<Kurt_DialogueManagerLvl2Ph2>().OpenDialogue(message, actor);
+    }
+}
